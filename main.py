ab92a9e61d31bac657cd34a0f01e35fe191d4511
Iliuta Mihai <mortvmis@gmail.com>
2018-09-29 16:52:45 +0300

    linked mongodb and flask

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 from flask import Flask, render_template
+from pymongo import MongoClient
+
 app = Flask(__name__)
+client = MongoClient('localhost', 27017)
+db = client['local']
+collection = db['phone']
 
 
-@app.route('/')
-def hello():
-    return render_template('index.html')
+@app.route("/")
+def home_page():
+    result = []
+    cursor = collection.find({})
+    for document in cursor:
+        result.append(document['name'])
+    return render_template("index.html", entries=result)
