a6796490845eacb617e8ef7ac9cf5deeb95e365b
Mihai <mihai-victor.iliuta@nokia.com>
2018-10-04 15:44:33 +0300

    added contact page

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 from pymongo import MongoClient
 
 app = Flask(__name__)
 client = MongoClient('localhost', 27017)
 db = client['local']
 collection = db['phone']
+collection2 = db['comments']
 
 
 @app.route("/")
@@ -29,7 +30,7 @@ def search_page():
     return render_template("category.html", brands=brands_list, result=result)
 
 
-@app.route("/<brand>")
+@app.route("/variable/<string:brand>")
 def variable_page(brand):
     result = []
     brands_list = collection.distinct('brand')
@@ -42,3 +43,28 @@ def variable_page(brand):
     for document in cursor:
         result.append([document['date'], document['brand'], document['model'], document['rating'], document['proc'], document['price']])
     return render_template("category.html", brands=brands_list, result=result)
+
+
+@app.route("/contact")
+def contact_page():
+    result = []
+    brands_list = collection.distinct('brand')
+    cursor = collection2.find({})
+
+    for document in cursor:
+        result.append([document['fname'], document['lname'], document['comment']])
+
+    return render_template("contact.html", brands=brands_list, result=result)
+
+
+@app.route("/contact", methods=['POST'])
+def submit_to_db():
+    entry = {}
+    entry["fname"] = request.form['firstname']
+    entry["lname"] = request.form['lastname']
+    entry["comment"] = request.form['comment']
+
+    if(entry["fname"] and entry["lname"] and entry["comment"]):
+        collection2.insert_one(entry)
+
+    return redirect(url_for('contact_page'))
